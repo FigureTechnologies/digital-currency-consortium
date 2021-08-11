5da1ae47b5d4e83e574a9ce0426bac787f08ae09
Rana DeWall <40402305+ranadewall@users.noreply.github.com>
2021-08-11 10:50:34 -0600

    Delete database/build directory

--- database/build/resources/main/db-init/db-init.sql
+++
@@ -1,5 +0,0 @@
-ALTER USER postgres PASSWORD 'password1';
-CREATE DATABASE usdfconsortium;
-GRANT ALL ON DATABASE usdfconsortium TO postgres;
-CREATE SCHEMA IF NOT EXISTS usdfconsortium AUTHORIZATION postgres;
-GRANT ALL ON SCHEMA usdfconsortium TO postgres;
