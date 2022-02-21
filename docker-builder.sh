67686d7a327562d7a6b373eed0f99a7652ddce83
Patrick SooHoo <patrick.soohoo@radialtheory.com>
2022-02-21 17:58:01 -0500

    Update github workflow, docker repository info

--- docker-builder.sh
+++ docker-builder.sh
@@ -7,9 +7,9 @@ gradle clean build -x test
 export CODE_VERSION="$(git rev-parse HEAD)-$(date +'%Y%m%d.%H%M%S')"
 export GithubBranch=$(git branch|grep '*'|cut -d' ' -f2)
 
-IMAGE_NAME="digital-currency-consortium-service"
+IMAGE_NAME="hosted-node-dcc"
 TAG="$GithubBranch_$CODE_VERSION"
-DOCKER_USER="radialtheory"
+DOCKER_USER="fin3technologies"
 
 docker build -f docker/Dockerfile-service --platform linux/x86_64 -t ${DOCKER_USER}/${IMAGE_NAME}:${TAG} .
 docker tag ${DOCKER_USER}/${IMAGE_NAME}:${TAG} ${DOCKER_USER}/${IMAGE_NAME}:latest
