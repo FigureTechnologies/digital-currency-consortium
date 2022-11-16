4caedfa539fc4724936e59f478e8c53f9be2b254
Jimmy Kang <jckang@gmail.com>
2022-11-16 15:20:20 -0500

    Remove unnecessary build dependencies after merge from main.

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,44 +1,14 @@
-import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
-
-
-configurations {
-    all {
-        exclude(group = "log4j")
-        resolutionStrategy.eachDependency {
-            if (requested.group == "org.apache.logging.log4j") {
-                useVersion("2.17.0")
-                because("CVE-2021-44228")
-            }
-        }
-    }
-}
+import org.springframework.boot.gradle.tasks.run.BootRun
 
 dependencies {
-    implementation(project(":database"))
-
-    api(Libraries.LogbackCore)
-    api(Libraries.LogbackClassic)
-    api(Libraries.LogbackJackson)
-
     implementation.let {
+        it(project(":database"))
+
         it(Libraries.FeignCore)
         it(Libraries.FeignJackson)
         it(Libraries.FeignSlf4j)
-        it(Libraries.Jackson) {
-            exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
-        }
-        it(Libraries.JacksonHubspot)
 
         it(Libraries.PbcProto)
-
-        it(Libraries.SpringBootDevTools)
-        it(Libraries.SpringBootActuator)
-        it(Libraries.SpringBootStartedJdbc)
-        it(Libraries.SpringBootStarterWeb)
-        it(Libraries.SpringBootStarterValidation)
-        it(Libraries.JavaxValidation)
         it(Libraries.PbcClient)
         it(Libraries.PbcHDWallet)
 
@@ -50,33 +20,16 @@ dependencies {
         it(Libraries.ScarletWebsocket)
         it(Libraries.ScarletMessageAdapter)
 
-        // ----- Misc -----
-        it(Libraries.Swagger2)
-        it(Libraries.SwaggerStarter)
-        it(Libraries.SwaggerUi)
-        it(Libraries.Flyway)
-        it(Libraries.Exposed)
-        it(Libraries.ExposedDao)
-        it(Libraries.ExposedJdbc)
-
+        // telemetry
+        it(Libraries.MicrometerPrometheusRegistry)
     }
-
-    testImplementation.let {
-        it(Libraries.JunitJupiterApi)
-        it(Libraries.JunitJupiterParams)
-        it(Libraries.JunitCommons)
-        it(Libraries.SpringBootStarterTest)
-        it(Libraries.Mockito)
-        it(Libraries.Mockk)
-        it(Libraries.TestContainersPostgres)
-        it(Libraries.TestContainers)
-        it(Libraries.TestContainersJunitJupiter)
-    }
-
-    testRuntimeOnly(Libraries.JunitJupiterEngine)
 }
-
 
 // Spring boot settings
 val profiles = System.getenv("SPRING_PROFILES_ACTIVE") ?: "development"
 
+tasks.getByName<BootRun>("bootRun") {
+    args = mutableListOf("--spring.profiles.active=$profiles")
+}
+
+springBoot.mainClass.set("io.provenance.digitalcurrency.consortium.ApplicationKt")
